ce0014a1c391438843f70bd888715b8760260f3f
Jonas Eira <jonas.eira@gmail.com>
2022-08-17 12:38:32 +0200

    Fixed exercise on last page of PDF so that is uses lambda expressions instead of anon. inner classes.

--- src/main/java/se/lexicon/exercise/Main.java
+++ src/main/java/se/lexicon/exercise/Main.java
@@ -3,9 +3,9 @@ package se.lexicon.exercise;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Main {
-
 
 
     public static void main(String[] args) {
@@ -16,45 +16,26 @@ public class Main {
         kakor.add(new Product("Kaka4", 1000.0, 0));
         kakor.add(new Product("Kaka5", 20.0, 10000));
         System.out.println("Products that are out of stock:");
-        findAndFilter(kakor, new Conditional<Product>() {
-            @Override
-            public boolean test(Product p) {
-                if(p.getStock() == 0){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }, new Action<Product>() {
-            @Override
-            public void execute(Product p) {
-                System.out.println(p);
-            }
-        });
+        findAndFilter(kakor, p -> p.getStock() == 0, p -> System.out.println(p));
 
         System.out.println("Products that are in price range 100.0 - 150.0 kr :");
-        findAndFilter(kakor, new Conditional<Product>() {
-            @Override
-            public boolean test(Product p) {
-                if (p.getPrice() >= 100.0 && p.getPrice() <= 150.0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }, new Action<Product>() {
-            @Override
-            public void execute(Product p) {
-                System.out.println(p);
-            }
-        });
+        findAndFilter(kakor, p -> p.getPrice() >= 100.0 && p.getPrice() <= 150.0, (Product p) -> System.out.println(p));
 
+        System.out.println("\nPrices before: ");
+        for (Product p : kakor) {
+            System.out.println(p.getProductName() + ": " + p.getPrice());
+        }
+        findAndFilter(kakor, p -> p.getPrice() <= 50.0, p -> p.setPrice(p.getPrice() * 1.50d));
+        System.out.println("\nPrices after: ");
+        for (Product p : kakor) {
+            System.out.println(p.getProductName() + ": " + p.getPrice());
+        }
 
     }
 
-    public static <F> void findAndFilter(Collection<F> things, Conditional<F> cond, Action<F> ac){
-        for(F thing : things){
-            if(cond.test(thing)){
+    public static <F> void findAndFilter(Collection<F> things, Conditional<F> cond, Action<F> ac) {
+        for (F thing : things) {
+            if (cond.test(thing)) {
                 ac.execute(thing);
             }
         }
